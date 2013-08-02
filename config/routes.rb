e919a4836788be406011dc820d1d21dbcf84bda3
Nestor Oscar Dominguez Diaz <nestor@nestor-desktop.(none)>
2013-08-02 03:12:23 -0300

    ajax nas rep de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Depot::Application.routes.draw do
 	root :to => 'store#index', :as => 'store'
 	
 	get "store/index" 
-	get "products/index", :as => "products"
+	
 	
 	resources :products, :carts, :line_items, :store
 end
