41f0942513add2c0cc55c4316a96240191fbc15e
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2013-08-08 02:58:30 -0300

    fin de checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Depot::Application.routes.draw do
+  
+
+
 	root :to => 'store#index', :as => 'store'
 	
 	get "store/index" 
 	
 	
-	resources :products, :carts, :line_items, :store
+	resources :products, :carts, :line_items, :store, :orders
 end
